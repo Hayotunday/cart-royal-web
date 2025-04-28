1fbe52c2f4f4305eb2e32cbb9e612fda5bf6f33d
Hayotunday <idowudanielayotunde@gmail.com>
2025-04-28 22:04:35 +0100

    created Shop page

---
+++ app/(root)/shop/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Shops = () => {
+  return (
+    <main className="p-14" id="shop">
+      <h1>Shops</h1>
+      <p>Welcome to the shop page!</p>
+    </main>
+  );
+};
+
+export default Shops;
