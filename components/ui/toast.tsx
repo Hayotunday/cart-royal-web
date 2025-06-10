e46bfaef62a7a0f641a30a2b85603754a4abad01
Dev Chris <hi.chrisdev@gmail.com>
2025-06-10 13:29:59 +0100

    Fixed: Type error on toast component

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,12 +1,19 @@
-import { CheckCircle, XCircle } from "lucide-react"
-import { toast } from "sonner"
+import { toast } from "sonner";
 
-export const showToast = ({ message, type = 'success' }) => {
+type ToastType = "success" | "info" | "warning" | "error";
+
+export const showToast = ({
+  message,
+  type = "success",
+}: {
+  message: string;
+  type?: ToastType;
+}) => {
   toast[type](message, {
     duration: 3000,
     style: {
-      width: 'fit-content',
-      minWidth: 'min-content'
-    }
-  })
-}
+      width: "fit-content",
+      minWidth: "min-content",
+    },
+  });
+};
