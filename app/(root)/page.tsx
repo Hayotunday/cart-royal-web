c9c280ccfe8ebdd2c5fd8c0bcd04d06d4ac7988b
Hayotunday <idowudanielayotunde@gmail.com>
2025-06-14 21:51:46 +0100

    commit to fix component name issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,45 @@ import React from "react";
 import Shop from "@/components/shared/shop";
 import Welcome from "@/components/layout/welcome";
 import Footer from "@/components/layout/footer";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col bg-gray-100 relative">
       <Welcome />
       <Shop />
+      <div className="flex flex-col md:flex-row items-start justify-between gap-6 md:gap-x-30 mx-auto">
+        <div className="">
+          <h1 className="text-xl font-semibold">Join our newsletter</h1>
+          <p className="text-[13px] text-[#6B7280] mt-2">
+            Register now to get latest updates on promotions & coupons. Don’t
+            worry, we don't spam!
+          </p>
+        </div>
+
+        <div className="mt-1 md:mt-0">
+          <div className="flex">
+            <Input
+              type="email"
+              placeholder="Enter your email address"
+              className="h-[48px] bg-white border-1 rounded-l-[8px] rounded-r-0"
+            />
+            <button
+              type="button"
+              className="bg-primary text-white w-[73px] rounded-r-[8px] font-semibold text-sm"
+            >
+              SEND
+            </button>
+          </div>
+          <div className="text-[12px] mt-2 ml-1">
+            By subscribing you agree to our{" "}
+            <span className="text-primary font-medium">
+              Terms & Conditions and Privacy & Cookies Policy.
+            </span>
+          </div>
+        </div>
+      </div>
+      <hr className="my-8 bg-gray-900" />
       <Footer />
     </div>
   );
--- components/layout/footers.tsx
+++ components/layout/footers.tsx
@@ -29,38 +29,6 @@ const Footer = () => {
     <Fragment>
       <div className="p-12 bg-[#F3F4F6] text-black">
         <div className="md:max-w-6xl mx-auto">
-          <div className="flex flex-col md:flex-row items-start justify-between gap-6 md:gap-x-30 mx-auto">
-            <div className="">
-              <h1 className="text-xl font-semibold">Join our newsletter</h1>
-              <p className="text-[13px] text-[#6B7280] mt-2">
-                Register now to get latest updates on promotions & coupons.
-                Don’t worry, we don't spam!
-              </p>
-            </div>
-
-            <div className="mt-1 md:mt-0">
-              <div className="flex">
-                <Input
-                  type="email"
-                  placeholder="Enter your email address"
-                  className="h-[48px] bg-white border-1 rounded-l-[8px] rounded-r-0"
-                />
-                <button
-                  type="button"
-                  className="bg-primary text-white w-[73px] rounded-r-[8px] font-semibold text-sm"
-                >
-                  SEND
-                </button>
-              </div>
-              <div className="text-[12px] mt-2 ml-1">
-                By subscribing you agree to our{" "}
-                <span className="text-primary font-medium">
-                  Terms & Conditions and Privacy & Cookies Policy.
-                </span>
-              </div>
-            </div>
-          </div>
-          <hr className="my-8 bg-gray-900" />
           <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:grid-rows-2 2xl:grid-rows-none 2xl:grid-cols-5 gap-y-6 gap-x-10">
             {/* Item 1 */}
             <div className="h-fit">
