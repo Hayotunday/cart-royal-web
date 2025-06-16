6e54e3b4ff2224bf2c390a1d27f68418db457472
Hayotunday <idowudanielayotunde@gmail.com>
2025-06-16 21:55:19 +0100

    worked on ad-bar component

--- components/layout/ad-bar.tsx
+++ components/layout/ad-bar.tsx
@@ -4,23 +4,22 @@ import React from "react";
 const AdBar = () => {
   return (
     <div className="w-full h-fit flex flex-row justify-end gap-5 items-center bg-gray-300 py-1 px-6">
-      <marquee
-        behavior="scroll"
-        direction="rtl"
-        className="text-gray-700 text-sm"
-      >
-        Advertisement
-      </marquee>
+      {/* Container for the scrolling text. It needs to clip its content and take available space. */}
+      <div className="flex-grow overflow-hidden">
+        <span className="marquee-text-ltr text-gray-700 text-sm">
+          Advertisement
+        </span>
+      </div>
 
       <Link
         href="/login"
-        className="text-black font-medium text-sm text-center"
+        className="text-black font-medium text-sm text-center text-nowrap"
       >
         Sign In
       </Link>
       <Link
         href="/register"
-        className="text-black font-medium text-sm text-center"
+        className="text-black font-medium text-sm text-center text-nowrap"
       >
         Sign Up
       </Link>
